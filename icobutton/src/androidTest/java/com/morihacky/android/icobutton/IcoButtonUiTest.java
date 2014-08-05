9bd6b63b1e031d131c7c6d6e2a099b469e5612a2
Kaushik Gopal <code@kaush.co>
2014-08-04 17:30:27 -0700

    test: add placeholder for test

---
+++ icobutton/src/androidTest/java/com/morihacky/android/icobutton/IcoButtonUiTest.java
@@ -0,0 +1,6 @@
+package com.morihacky.android.icobutton;
+
+public class IcoButtonUiTest {
+
+    // TESTME: check all buttons are visible (would have helped test case where totalWidth = getMode)
+}
